25920d3c16d7fa30a15312a1f9ef989b271780d3
Guilherme Leao <leaob7@gmail.com>
2021-12-19 10:09:42 -0300

    poster design 1

--- src/Pages/MoviePage.js
+++ src/Pages/MoviePage.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native';
 import { styles } from '../styles/styleMoviePage';
 import { requestFilmDetails } from '../utils/requests';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function MoviePage({ route }) {
   const { id } = route.params;
@@ -16,22 +17,63 @@ export default function MoviePage({ route }) {
     <>
     {movieDetails ? <View style={ styles.moviePageContainer }>
       <View style={ styles.posterContainer }>
-        <Text>{movieDetails.title}</Text>
-        <Text>{`IMDb ${movieDetails.vote_average}`}</Text>
-        {movieDetails.genres.map((genre, index) => (
-          <Text key={ index }>
-            {genre.name}
-          </Text>
-        ))}
-        <Text>
-          {movieDetails.release_date.slice(0, 4)}, 
-          {movieDetails.production_countries[0].iso_3166_1},
-          {movieDetails.runtime},
-        </Text>
+        <ImageBackground
+          source={{uri: `https://image.tmdb.org/t/p/original/${movieDetails.poster_path}`}}
+          resizeMode="stretch"
+          style={ styles.poster }
+          imageStyle={{ borderBottomLeftRadius: 20, borderBottomRightRadius: 20 }}
+        >
+          <View style={ styles.infoView }>
 
-        <TouchableOpacity>
-          <Text>Wacth for free</Text>
-        </TouchableOpacity>
+            <Text style={ styles.movieTitle }>{movieDetails.title}</Text>
+
+            <View style={ styles.ratingView }>
+              <Text style={ styles.ratingText }>
+                IMDb
+              </Text>
+              <Text style={ { color: "#fff", fontSize: 15, fontWeight: "bold", marginTop: 3 } }>
+                {movieDetails.vote_average}
+              </Text>
+            </View>
+
+            <View style={ styles.genreView }>
+              {movieDetails.genres.map((genre, index) => (
+                <Text style={ styles.posterInfoText } key={ index }>
+                  {genre.name}, 
+                </Text>
+              ))}
+            </View>
+
+            <View style={ styles.genreView }>
+              <Text style={ styles.posterInfoText }>
+                {`${movieDetails.release_date.slice(0, 4)}, ${movieDetails.production_countries[0].iso_3166_1}, ${movieDetails.runtime}`}
+
+              </Text>
+            </View>
+
+            <View style={ styles.watchBtnContainer }>
+
+              <TouchableOpacity style={ styles.watchBtn }>
+
+                <Text style={ styles.watchText }>Wacth for free</Text>
+                <Icon
+                  name="play-circle"
+                  color="#d1b100" size={25} 
+                  style={{ marginRight: 15, marginTop: 2 }}
+                />
+
+              </TouchableOpacity>
+
+              <View style={ styles.posterIcon }>
+                <Icon name="star" color="#fff" size={20} style={{ marginRight: 10 }} />
+                <Icon name="share-alt" color="#fff" size={20} />
+              </View>
+
+            </View>
+
+          </View>
+
+        </ImageBackground>
       </View>
 
       <View>
--- src/styles/styleMoviePage.js
+++ src/styles/styleMoviePage.js
@@ -5,6 +5,69 @@ export const styles = StyleSheet.create({
     backgroundColor: "#331177",
   },
   posterContainer: {
-
+    borderRadius: 20,
   },
+  poster: {
+    justifyContent: "center",
+    height: 400,
+    width: "100%",
+    marginBottom: 30,
+  },
+  ratingView: {
+    flexDirection: 'row',
+  },
+  ratingText: {
+    fontSize: 20,
+    color: "purple",
+    fontWeight: "bold",
+    marginRight: 5,
+    textAlign: "center",
+  },
+  infoView: {
+    marginTop: "35%",
+    margin: 30,
+  },
+  movieTitle: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 7,
+  },
+  posterInfoText: {
+    color: "#fff",
+    fontSize: 15,
+    marginTop: 7,
+    marginRight: 5,
+  },
+  genreView: {
+    flexDirection: "row",
+    marginRight: 5,
+  },
+  watchBtnContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  watchBtn: {
+    flexDirection: "row",
+    backgroundColor: '#6325df',
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 5,
+    borderRadius: 15,
+    textAlign: "center",
+    marginTop: 30,
+    width: 200,
+  },
+  watchText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#fff",
+    margin: 5,
+    marginLeft: 15,
+  },
+  posterIcon: {
+    flexDirection: "row",
+    marginTop: 30,
+  }
 })
